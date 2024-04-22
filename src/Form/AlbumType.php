d72cb7551b7ec9ddba83a488b204e7325937423c
marina <marinacheca11@gmail.com>
2024-04-22 16:32:55 +0200

    Correccions

--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -7,6 +7,8 @@ use App\Entity\Band;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,8 +17,8 @@ class AlbumType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('releasedAt', null, [
+            ->add('title', TextType::class)
+            ->add('releasedAt', DateType::class, [
                 'widget' => 'single_text',
             ])
             ->add('cover')
