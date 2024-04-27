55ee6c47b266d9772dbb0848040612dce1ac435c
marina <marinacheca11@gmail.com>
2024-04-27 13:32:19 +0200

    Afegir al _header 'Bandes'

--- src/Controller/BandController.php
+++ src/Controller/BandController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/band')]
+#[Route('admin/band')]
 class BandController extends AbstractController
 {
     #[Route('/', name: 'app_band_index', methods: ['GET'])]
