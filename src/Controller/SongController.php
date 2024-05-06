ffdb227148dd409f898645038c6892bd0d406a11
marina <marinacheca11@gmail.com>
2024-05-06 16:58:59 +0200

    Botóns Detalls i Like de Home alineats

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/admin')]
+#[Route('/admin/songs')]
 class SongController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
